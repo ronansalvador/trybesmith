04175b865d561f8b30dfdd4f61f796bb03e932db
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-11-04 16:14:46 -0300

    feat(route users): create route users

--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import productRouter from './products.route';
+import userRouter from './users.route';
 
 const router = Router();
 
 router.use('/products', productRouter);
+router.use('/users', userRouter);
 
 export default router;
