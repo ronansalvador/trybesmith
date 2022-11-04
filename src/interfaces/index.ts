2dd67ae5bff04e42077b46d33b1d23e8279dfb83
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-11-04 15:52:34 -0300

    feat(users): create interface and users model

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -3,3 +3,11 @@ export interface IProduct {
   name: string;
   amount: string;
 }
+
+export interface IUser {
+  id?: number;
+  username: string;
+  classe: string;
+  level: number;
+  password: string;
+}
