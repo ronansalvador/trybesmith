2a91dcd6453fb34fdbbd412edf0dd5c4e5516dee
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-11-04 17:15:10 -0300

    feat(route orders): create route orders

--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import productRouter from './products.route';
 import userRouter from './users.route';
+import orderRoute from './orders.route';
 
 const router = Router();
 
 router.use('/products', productRouter);
 router.use('/users', userRouter);
+router.use('/orders', orderRoute);
 
 export default router;
